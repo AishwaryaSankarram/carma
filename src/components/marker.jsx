9e4697deb1e678953cea0a3300884b5e815af254
Guru murthi <gurumurthi005@gmail.com>
2018-02-24 16:05:00 +0530

    UI Changes: Apply custom colours for markers - SVG

--- src/components/marker.jsx
+++ src/components/marker.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Marker } from "react-google-maps";
+import {MarkerImg} from "./MarkerImg.jsx";
 export class MyMarker extends React.Component {
 	constructor(props) {
 		super(props);
@@ -15,7 +16,14 @@ export class MyMarker extends React.Component {
 
 	render() {
 		let self = this;
-		let icon = self.props.icon || ""
+		let google = window.google;
+		var svg = [
+                '<?xml version="1.0"?>',
+                    '<svg  viewBox="0 0 17.93 27.11" version="1.1" xmlns="http://www.w3.org/2000/svg">',
+                        '<path d="M692.49,2226.38a9,9,0,0,0-9,9c0,2,1.27,5.24,3.89,9.93,1.85,3.32,3.67,6.08,3.75,6.2l1.33,2,1.33-2c0.08-.12,1.9-2.88,3.75-6.2,2.62-4.69,3.89-7.93,3.89-9.93a9,9,0,0,0-9-9h0Zm0,13.55a4.65,4.65,0,1,1,4.65-4.65,4.65,4.65,0,0,1-4.65,4.65h0Zm0,0" transform="translate(-683.52 -2226.38)" style="fill:'+ '#00FF00' +'"/>',
+                    '</svg>'
+                ].join('\n');
+	  let icon = { url: 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(svg), scaledSize: new google.maps.Size(30, 30) };
 		let title = self.props.title || "";
 		let zIndex = self.props.allowEdit ? 150 : 137 ;
 		let markers = this.props.markerPos.map(function(marker) {
