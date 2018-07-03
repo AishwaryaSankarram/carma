c4ef7ab6fb3f810763b1a03fa66ba8f7404e046d
Aishwarya S <aishwarya@carmanetworks.com>
2018-07-03 16:04:39 +0530

    Point API URL to gateway IP

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 module.exports = {
-    baseUrl: "http://localhost:8090/",
-    remoteIp: "192.168.1.13",
+    baseUrl: "http://183.82.32.121:8090/",
+    remoteIp: "192.168.1.5",
     remotePath:"/tmp/",
-    remotePass:"xxxxxx",
-    remoteUser:"yyyyyy"
+    remotePass:"P@ssc0de",
+    remoteUser:"murali"
 }
